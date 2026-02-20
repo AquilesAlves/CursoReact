4b40971489ab41582dde1e8bf773898f85a3c394
Aquiles Alves <aquilespereira789@gmail.com>
2026-02-20 02:28:34 -0300

    as

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,5 +1,14 @@
 function Titulo() {
-    return <h1>Oi eu sou um título</h1>
+
+    let nome = 'Aquiles Alves'
+
+  return (
+    <div>
+      <h1>Oi eu sou Fulano</h1>
+      
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam esse possimus inventore accusamus natus illo ex nemo nobis ipsa. Dolorem at ipsa voluptatibus maxime eveniet natus maiores necessitatibus tempore eaque?</p>
+    </div>
+  );
 }
 
-export default Titulo
+export default Titulo;
